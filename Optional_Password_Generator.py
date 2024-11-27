3f8bd7cb951747ea95cf6904ea5d2e19208332f4
Lucie Wu <wu.lucie@gmail.com>
2024-11-27 18:33:36 +0100

    update password generator after correction

--- Optional_Password_Generator.py
+++ Optional_Password_Generator.py
@@ -1,7 +1,8 @@
 """
 Generate a password of the size given by a user.
 """
-import random
+# requires 2 libraries
+import random 
 import string
 
 size_user = int(input("Enter the size of your desired password"))
@@ -13,13 +14,15 @@ while password_level not in {"Y","N"}:
 password_list = []
 password = ""
 
-'''version for a password with only numbers'''
+'''
+# IF version for a password with only numbers
 if password_level == "N":
     # password_list.append(random.choice(string.ascii_letters,size_user-1)) marche pas avec un 2nd argument
     for i in range(size_user):
         #password_list.append(random.randint(0,9))
         password_list.append(random.choice(string.digits))
     password = "".join(str(p) for p in password_list)
+# ELIF strong password
 elif password_level == "Y": #version for a strong password i.e with letters, numbers, symbols
     for i in range(size_user):
         if random.randint(0,9)%2 == 0: # scenario 1: chiffre ou symbol
@@ -29,6 +32,30 @@ elif password_level == "Y": #version for a strong password i.e with letters, num
                 password_list.append(random.choice('!$%&()*+,-.:;<=>?@[]^_`{|}~'))
         else: # scenario 2: nombre
             password_list.append(random.choice(string.ascii_letters))
+    password = "".join(str(p) for p in password_list)
+else:
+    print("password level not defined!")
+'''
+
+''' solution optimizes the above by defining a list of ALL characters 
+including numbers, letters and symbols
+    chars = string.ascii_letters + string.digits + string.punctuation
+then use
+    password_list.append(random.choice(chars))
+TRY IT OUT BY running code below and putting above tentative in ''' '''
+    '''
+
+if password_level == "N":
+    # password_list.append(random.choice(string.ascii_letters,size_user-1)) marche pas avec un 2nd argument
+    for i in range(size_user):
+        #password_list.append(random.randint(0,9))
+        password_list.append(random.choice(string.digits))
+    password = "".join(str(p) for p in password_list)
+# ELIF strong password
+elif password_level == "Y": #version for a strong password i.e with letters, numbers, symbols
+    for i in range(size_user):
+        chars = string.ascii_letters + string.digits + string.punctuation
+        password_list.append(random.choice(chars))
     password = "".join(str(p) for p in password_list)
 else:
     print("password level not defined!")
